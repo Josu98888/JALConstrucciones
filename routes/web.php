879264c895d05e06273e57df3773c53c4fdc0551
Josué Aquino <aquinojosue889@gmail.com>
2025-03-27 19:29:41 -0300

    Se creó el CategoryController de tipo resource con su respectiva ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Rutas de user
 Route::post('/api/login', [UserController::class, 'login']);
 Route::post('/api/user/update', [UserController::class, 'update']);
 Route::get('/api/user/avatar/{filename}', [UserController::class, 'getImage']);
 Route::get('/api/user/detail/{id}', [UserController::class, 'detail']);
+
+//Rutas de category
+Route::resource('/api/category', CategoryController::class);
