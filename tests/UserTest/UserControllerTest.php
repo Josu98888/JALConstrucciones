3af4aa5de58a4e6aefeac196e984976bbfd09458
Josué Aquino <aquinojosue889@gmail.com>
2025-05-17 17:09:09 -0300

    Se creó el test getImage en UserControllerTest

--- tests/UserTest/UserControllerTest.php
+++ tests/UserTest/UserControllerTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tests\UserTest;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase as TestsTestCase;
 use Tests\Traits\CreateUser as TraitsCreateUser;
@@ -56,7 +58,7 @@ class UserControllerTest extends TestsTestCase
         // llamada
         $response = $this->withHeaders([
             'Authorization' => $token,
-        ])->postJson('/api/user/update', $data); 
+        ])->postJson('/api/user/update', $data);
 
         // verificación
         // $response->dump();
@@ -68,5 +70,24 @@ class UserControllerTest extends TestsTestCase
                 'email' => 'testUserUpdated@email.com',
             ]
         ]);
+    }
+
+    #[Test]
+    public function getImage()
+    {
+        // preparación 
+        $filename = 'test-image.jpg';
+        $file = UploadedFile::fake()->image($filename);                     // Crea un archivo de imagen falso
+        Storage::disk('public')->putFileAs('users', $file, $filename);      // Guarda el archivo en el disco público
+
+        // llamada 
+        $response = $this->get("/api/user/avatar/$filename");
+
+        // verificación
+        $response->assertStatus(200);
+        $response->assertHeader('Content-Type', 'image/jpeg');
+
+        // limpieza
+        Storage::disk('public')->delete("users/{$filename}");
     }
 }
