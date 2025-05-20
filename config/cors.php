20c5df3f1c7a9c483cc4b3854a17acf2e611b0db
Josué Aquino <aquinojosue889@gmail.com>
2025-05-20 16:04:57 -0300

    Refactor de la url en el cors

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://frontend-jal-construcciones-ba8c.vercel.app/', 'https://frontend-jal-construcciones-ba8c.vercel.app/'],
+    'allowed_origins' => ['https://frontend-jal-construcciones.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
